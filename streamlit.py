e859d5457c507be3843269428457ac52e42b1746
Niroskl <nirskalka@gmail.com>
2025-11-21 14:36:21 +0200

    Adjust scene parameters and add active loop
    Updated car and track dimensions, added scene center and a simple loop to keep the scene active.

--- streamlit.py
+++ streamlit.py
@@ -1,22 +1,27 @@
 from vpython import *
 
-scene = canvas(title="🏎️ משחק מכוניות 3D", width=800, height=600)
+# יצירת סצנה
+scene = canvas(title="🏎️ משחק מכוניות 3D", width=800, height=600, center=vector(0,0,0))
 
 # מסלול
-track = box(pos=vector(0,0,0), size=vector(20,0.5,5), color=color.gray(0.5))
+track = box(pos=vector(0,0,0), size=vector(40,0.5,10), color=color.gray(0.5))
 
 # מכונית
-car = box(pos=vector(-8,0.5,0), size=vector(2,1,1), color=color.red)
+car = box(pos=vector(-18,0.5,0), size=vector(2,1,1), color=color.red)
 
-# פונקציה לשליטה
+# פונקציה לשליטה במכונית
 def move_car(evt):
     if evt.key == "up":
         car.pos.x += 0.5
+    elif evt.key == "down":
+        car.pos.x -= 0.5
     elif evt.key == "left":
         car.pos.z -= 0.5
     elif evt.key == "right":
         car.pos.z += 0.5
-    elif evt.key == "down":
-        car.pos.x -= 0.5
 
 scene.bind("keydown", move_car)
+
+# לולאה פשוטה לשמירה על סצנה פעילה
+while True:
+    rate(60)
